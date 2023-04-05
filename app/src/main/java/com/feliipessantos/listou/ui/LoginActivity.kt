a139bd757f252b9e4a8cb70310256af5ecbbc8f1
feliipessantos <lipe_silva_santos@hotmail.com>
2023-04-05 17:11:04 -0300

    add validations

--- app/src/main/java/com/feliipessantos/listou/ui/LoginActivity.kt
+++ app/src/main/java/com/feliipessantos/listou/ui/LoginActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatActivity
+import com.feliipessantos.listou.R
 import com.feliipessantos.listou.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
@@ -15,6 +16,56 @@ class LoginActivity : AppCompatActivity() {
 
         setupViewSwitcher()
         _binding.btLogin.setOnClickListener {
+            validateLogin()
+        }
+
+        _binding.BtRegister.setOnClickListener {
+            validateRegister()
+        }
+    }
+
+    private fun validateRegister() {
+        if (_binding.TextInputRegisterName.text.isNullOrEmpty()) {
+            _binding.EditName.apply {
+                isHelperTextEnabled = true
+                helperText = "Please fill your Name"
+                boxStrokeColor = getColor(R.color.red)
+            }
+        }
+        if (_binding.TextInputRegisterEmail.text.isNullOrEmpty()) {
+            _binding.EditEmailRegister.apply {
+                isHelperTextEnabled = true
+                helperText = "Please fill your email"
+                boxStrokeColor = getColor(R.color.red)
+            }
+        }
+        if (_binding.TextInputRegisterPass.text.toString().length < 6) {
+            _binding.EditPasswordRegister.apply {
+                isHelperTextEnabled = true
+                helperText = "Your password must have min 6 characters"
+                boxStrokeColor = getColor(R.color.red)
+            }
+        } else {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun validateLogin() {
+        if (_binding.TextInputEmail.text.isNullOrEmpty()) {
+            _binding.EditEmail.apply {
+                isHelperTextEnabled = true
+                helperText = "Please fill your email"
+                boxStrokeColor = getColor(R.color.red)
+            }
+        }
+        if (_binding.TextInputPass.text.isNullOrEmpty()) {
+            _binding.EditPassword.apply {
+                isHelperTextEnabled = true
+                helperText = "Please fill your password"
+                boxStrokeColor = getColor(R.color.red)
+            }
+        } else {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
